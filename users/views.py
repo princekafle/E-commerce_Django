4e51147f0b086e97b19ad2232299ca539ed0d9e1
Prince Kafle <89830489+princekafle@users.noreply.github.com>
2024-07-23 22:11:56 +0545

    Update views.py

--- users/views.py
+++ users/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from . forms import *
 from . models import *
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+from . filters import *
 
 
 # Create your views here.
@@ -20,16 +23,38 @@ def homepage(request):
     return render(request, 'users/index.html', context)
 # Product vanne model bata sabai data lai product vanne ma access garyo ani context ma rakhera html ma render garera pathayo
 def productpage(request):
-    product = Product.objects.all().order_by('-id')[:8]
+    user = request.user.id
+    items = Cart.objects.filter(user= user)
+    # mathy ko duita line chai cart iems ko nbr display garauna lai rakheko 
+
+    product = Product.objects.all().order_by('-id')
+    product_filter = ProductFilter(request.GET, queryset = product)
+    product_new = product_filter.qs
+
+    pages = Paginator(product_new,2)
+    pagenumber = request.GET.get('page')
+    # pagenumber = request.GET.get('page') le url bata page number linxa 
+    productfinal = pages.get_page(pagenumber)
+
+    # productfinal = paginator.get_page(pagenumber) le pagenumber ma vayeko product dekhauxa productfinal ko through bata
+    
 
     context = {
-        'product': product
+        'product':productfinal,
+        'product_filter': product_filter,
+        'items': items
+        
     }
     return render(request, 'users/products.html', context)
 
 def product_detail(request, product_id):
+    user = request.user.id
+    items = Cart.objects.filter(user= user)
     product = Product.objects.get(id=product_id)
-    context = {'product': product}
+    context = {
+        'product': product,
+        'items': items
+               }
     return render(request, 'users/productdetail.html', context)
 
 def user_register(request):
@@ -76,15 +101,84 @@ def logout_user(request):
     logout(request)
     return redirect('/login')
 
+@login_required
 def add_to_cart(request, product_id):
     product = Product.objects.get(id = product_id)
     user = request.user
+    # This retrieves the current logged-in user from the request
 
-    check_item_presence = Cart.objects.filter(user= user, product= product)
+    check_item_presence = Cart.objects.filter(user=user, product=product)
+    # yesle cart ma vayeko product ra user ani add to cart request garne user ra product lai check garxa
+
     if check_item_presence:
-        messages.add_message(request, messages.ERROR, 'Product is already presented in the cart')
+        messages.add_message(request, messages.ERROR, f'{product.product_name} is already presented in the cart')
+        return redirect(f'/productdetail/{product_id}')
     else:
-        Cart.objects.create(user=user, product =product)
-        messages.add_message(request, messages.SUCCESS, 'product is added successfully in cart')
-        return redirect('/cart')
+       cart=  Cart.objects.create(user= user, product = product)
+       if cart:
+            messages.add_message(request, messages.SUCCESS, 'product added successfully in cart')
+            return redirect('/cart')
+       else:
+            messages.add_message(request, messages.ERROR,'Error while adding in cart')
+@login_required
+def viewcart(request):
+    user = request.user.id
+    items = Cart.objects.filter(user= user)
+    # mathy ko line le chai jun user le cart create gareko xa tesle maatra cart herna pauxa
+    context = {
+        'items': items
+    }
+    return render(request, 'users/cart.html', context)
+
+@login_required
+def deletecart(request, cart_id):
+    item= Cart.objects.get(id = cart_id)
+    item.delete()
+    messages.add_message(request, messages.ERROR, 'Product deleted form the cart ')
+    return redirect('/cart')
+
+@login_required
+def order(request, product_id, cart_id):
+    user = request.user
+    product = Product.objects.get(id = product_id)
+    cart = Cart.objects.get(id = cart_id)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        quantity = request.POST.get('quantity')
+        price = product.product_price
+        total_price = int(quantity)*int(price)
+        contact_no = request.POST.get('contact_no')
+        address = request.POST.get('address')
+        payment_method = request.POST.get('payment_method')
+
+        order = Order.objects.create(
+            user = user,
+            product = product,
+            quantity = quantity,
+            total_price = total_price,
+            contact_no = contact_no,
+            address = address,
+            payment_method = payment_method
+
+        )
+        # //mathy request.method == 'POST': bata aako user le haleko data chai model ma haleko ho
+        # quantity = quantity vaneko quantity vaneko form bata ako data ra arko quantity vaneko model ma banako quantity
+        if order.payment_method == 'Cash on Delivery':
+            cart.delete()
+            messages.add_message(request, messages.SUCCESS, 'your order has been successfully ordered')
+            return redirect('/myorder')
+    context = {
+        'form': OrderForm
+    }
+    return render(request, 'users/order.html', context)
+@login_required
+def myorder(request):
+    user = request.user
+    order= Order.objects.filter(user=user)
+    context = {
+        'order': order
+    }
+    return render(request, 'users/myorder.html', context)
+    
     
